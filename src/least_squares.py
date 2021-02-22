ab6961990638242772ce9b30fa910847222fa8ec
Jean-Paul <jeanpaul.ivan.academic@gmail.com>
2021-02-22 09:00:38 +0100

    Improved documentation of least_squares.

--- src/least_squares.py
+++ src/least_squares.py
@@ -2,6 +2,28 @@ import numpy as np
 
 
 class least_squares:
+    """ The least_squares class is a convenient representation of a
+    least-squares problem instance. That is, for what x do we minimise
+    ||Ax - b||^2 ?
+
+    A least_squares object requires an A matrix and a b vector:
+    ls = least_squares(A, b)
+
+    Methods (see each method for more details):
+
+    __call__(self, x):
+        Returns the squared norm ||Ax -b||^2 for the given x.
+
+    solve_minimum(self):
+        A wrapper around the np.linalg.lstsq function which returns a
+        dictionary:
+        {
+            "x*": minimiser
+            "r": residuals, if appropriate
+            "rk": rank of A
+            "s": singular values of A
+        }
+    """
     def __init__(self, A, b):
         """ Create a least squares function using the given parameters.
         Instances of this class can be called with x as an argument to return
@@ -25,6 +47,12 @@ class least_squares:
             x: 2-d numpy matrix (column vector)
         Returns:
             || A*x - b ||_2^2
+
+        A = np.array([[1, 2], [3, 4]])
+        b = np.array([6, 7])
+        ls = least_squares(A, b)
+        ls([1, 1])
+        >> 9.0
         """
         _x = x
         if not isinstance(_x, np.ndarray):  # if not numpy array try to convert
@@ -40,7 +68,7 @@ class least_squares:
 
     def solve_minimum(self):
         """ Finds the x* which minimises the least-squares problem instance.
-        This uses np.linalg.lstsq.
+        See numpy.linalg.lstsq for more details.
 
         Args:
             None
@@ -50,6 +78,42 @@ class least_squares:
                 "r": residuals, if appropriate
                 "rk": rank of A
                 "s": singular values of A
+            }
+
+        # determined system
+        A = np.array([[1, 2], [3, 4]])
+        b = np.array([6, 7])
+        ls = least_squares(A, b)
+        ls.solve_minimum()
+        >> {
+            'x*': array([[-5], [ 5.5]]),
+            'r': array([], dtype=float64),
+            'rk': 2,
+            's': array([5.4649857 , 0.36596619])
+            }
+
+        # overdetermined system
+        A = np.array([[1, 2], [3, 4], [3, 5]])
+        b = np.array([6, 7, 8])
+        ls = least_squares(A, b)
+        ls.solve_minimum()
+        >> {
+            'x*': array([[-1.78571429], [ 2.92857143]]),
+            'r': array([5.78571429]),
+            'rk': 2,
+            's': array([7.98626929, 0.4685113 ])
+            }
+
+        # underdetermined system, will return one possible minimiser
+        A = np.array([[1, 2]])
+        b = np.array([6])
+        ls = least_squares(A, b)
+        ls.solve_minimum()
+        >> {
+            'x*': array([[1.2], [2.4]]),
+            'r': array([], dtype=float64),
+            'rk': 1,
+            's': array([2.23606798])
             }
         """
         _soln = np.linalg.lstsq(self.A, self.b, rcond=None)
