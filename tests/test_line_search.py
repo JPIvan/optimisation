78a7cfb50aa9d766bb941e4b905b722950523bd2
Jean-Paul <jeanpaul.ivan.academic@gmail.com>
2021-02-24 10:35:48 +0100

    Added test for golden section search
    - Test checks n-dimensional case under a well-specified problem.

--- tests/test_line_search.py
+++ tests/test_line_search.py
@@ -3,6 +3,7 @@ from pytest import approx, raises
 
 import context  # noqa
 from src import line_search
+from src.least_squares import least_squares
 
 
 class TestGoldenSection:
@@ -23,3 +24,40 @@ class TestGoldenSection:
             dx=-2,  # search direction: -f'(3)
         )
         assert solution['x'] == approx(4)
+
+    def test_correct_nd(self):
+        """ Check if n-dimensional problems which are well specified are solved
+        correctly.
+        """
+        for n in range(2, 10):
+            A = np.random.uniform(
+                low=-1,
+                high=1,
+                size=(n, n)
+            )
+            b = np.random.uniform(
+                low=-1,
+                high=1,
+                size=(n, 1)
+            )
+            LS = least_squares(A, b)  # Least squares instance ||Ax-b||^2
+
+            x0 = np.random.uniform(
+                low=-1,
+                high=1,
+                size=(n, 1)
+            )  # pick random starting point
+            dx = 2*A.T@A@x0 - 2*A.T@b  # derivative of x^TA^TAx-2x^TA^Tb-b^Tb
+
+            solution = line_search.goldensection(
+                func=lambda x: LS(x),
+                x=x0,
+                dx=-dx,
+            )
+
+            eps = 1e-3  # small deviation
+            # now check that we have found the minimum in this search direction
+            # since function is convex we just have to check small deviations
+            # from the solution
+            assert LS(solution['x']) < LS(solution['x'] + eps*dx)
+            assert LS(solution['x']) < LS(solution['x'] - eps*dx)
