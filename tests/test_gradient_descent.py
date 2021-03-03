3a117cf0f6d2ae3cee7d19c5e1401378be79c955
Jean-Paul <jeanpaul.ivan.academic@gmail.com>
2021-03-03 11:12:03 +0100

    Added test: check jacobian for integer polynomials
    - Checks for a failures that can occur when arrays are not upcasted

--- tests/test_gradient_descent.py
+++ tests/test_gradient_descent.py
@@ -43,3 +43,32 @@ class TestCreateJacobian:
             for _ in range(10):  # 10 random points on function
                 x = np.random.uniform(low=-10, high=10, size=(1, 1))
                 assert numericaljac(x).item() == approx(_polyderiv(x))
+
+    def test_create_jac_1d_integers(self):
+        """ Numpy arrrays do not upcast when individual elements are
+        modified. Verify that the jacobian creator does not fail when
+        passed a function defined with integers only.
+
+        example:
+            A = np.array([1, 2])  # created with dtype=int32 by default
+            A[0] = 1.1  # will not upcast result will be array([1, 2])
+        """
+        for _ in range(10):  # try 10 random polynomials
+            polydegree = np.random.randint(2, 5)
+            k = np.random.randint(low=-5, high=5, size=polydegree+1)
+            # some random integer coefficients for the polynomial
+
+            def _intpoly(x):
+                _x = np.array([x**n for n in range(polydegree+1)]).flatten()
+                return np.sum(k*_x)
+
+            def _intpolyderiv(x):
+                _k = k[1:]
+                _x = np.array([(n+1)*x**n for n in range(polydegree)])
+                _x = _x.flatten()
+                return np.sum(_k*_x)
+
+            numericaljac = gradient_descent._create_jac(_intpoly)
+            for _ in range(10):  # 10 random integer points on function
+                x = np.random.randint(low=-10, high=10, size=(1, 1))
+                assert numericaljac(x).item() == approx(_intpolyderiv(x))
