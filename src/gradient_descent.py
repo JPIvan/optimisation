Bugfix: increased relative precision
- jacobian would not be within 1e-6 on some reasonable polynomials
- in theory central difference has precision ~1e-8 with delta 1e-4
- failures still occured on tests with relative tolerance 1e-6
- expect relative precision ~1e-12 now

src/gradient_descent.py
@@ -23,9 +23,9 @@ def _create_jac(func):
 
         normx = np.linalg.norm(x)
         if normx == 0:
-            delta = 1E-4  # prevent division by 0 later
+            delta = 1E-6  # prevent division by 0 later
         else:
-            delta = 1E-4*normx  # scale step-size appropriately
+            delta = 1E-6*normx  # scale step-size appropriately
         jacx = np.zeros_like(x, dtype=float)
         for i in range(x.shape[0]):
             dxi = np.zeros_like(x, dtype=float)